0161d8f619a62dd8f019a377c82f0adacc9be588
John U <arjorb@gmail.com>
2023-04-22 18:01:08 +0200

    update the Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 
-export default function Home() {
+ const Home = () => {
   return (
     <main >
       <h1 className="text-3xl font-bold flex justify-center text-[#029994]">BuzzHub</h1> 
     </main>
   )
 }
+
+export default Home
